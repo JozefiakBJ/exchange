72740ebf322fafa5375502d325a8c17fbe9d4fad
Bartek Józefiak <93558443+jozefiakBJ@users.noreply.github.com>
2021-11-29 15:56:11 +0100

    swagger

---
+++ src/main/java/com/jozefiak/exchange/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.jozefiak.exchange.config;
+
+
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi
+                .builder()
+                .group("Api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+}
